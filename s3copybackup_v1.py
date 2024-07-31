19c28d77459245f15c2311438bce1b2162a97d20
Lucasliuzao <lucasisrael85@gmail.com>
2024-07-31 09:46:36 -0300

    Add msg arquivo existente

--- s3copybackup_v1.py
+++ s3copybackup_v1.py
@@ -5,7 +5,7 @@ s3 = boto3.client('s3')
 sns = boto3.client('sns')
 
 # ARN do tópico do SNS criado
-sns_topic_arn = 'arn:aws:sns:us-east-1:completa com o arn do sns'  # Substitua pelo ARN do seu tópico SNS
+sns_topic_arn = 'arn:aws:sns:us-east-1:942569085084:BucketCopyCorporeRM'  # Substitua pelo ARN do seu tópico SNS
 
 def lambda_handler(event, context):
     source_bucket = 'sfacopytest1'
@@ -27,14 +27,31 @@ def lambda_handler(event, context):
                     latest_object = most_recent_in_prefix
         
         if latest_object:
-            copy_source = {
-                'Bucket': source_bucket,
-                'Key': latest_object['Key']
-            }
-            
+            try:
+                # Verificar se o objeto já foi copiado para o bucket de destino
+                s3.head_object(Bucket=destination_bucket, Key=latest_object['Key'])
+                # Se o objeto já existe, notificar que o backup já foi feito
+                message = f"O backup '{latest_object['Key']}' já existe no bucket {destination_bucket}. Nenhuma ação necessária."
+                subject = "Backup Já Existente"
+                sns.publish(TopicArn=sns_topic_arn, Message=message, Subject=subject)
+                return {
+                    'statusCode': 200,
+                    'body': message
+                }
+            except s3.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == '404':
+                    # Objeto não encontrado, continuar com a cópia
+                    pass
+                else:
+                    # Outro erro ocorreu
+                    raise
+
             # Copiar o objeto para o bucket de destino mantendo o mesmo caminho
             s3.copy_object(
-                CopySource=copy_source,
+                CopySource={
+                    'Bucket': source_bucket,
+                    'Key': latest_object['Key']
+                },
                 Bucket=destination_bucket,
                 Key=latest_object['Key']  # Mantém o mesmo nome e caminho no bucket de destino
             )
